8dac307473764cce724a17a864da46580275a448
maria483 <mariacstein@gmail.com>
2023-03-28 15:56:04 -0400

    started working on photo intent
    - declare FileProvider as a ContentProvider hooked up to a specific authority, do this to set device storage location for images (edit manifests/AndroidManifest.xml)
    
    - tell FileProvider which files it is exposing by making an xml file that maps the root path of private storage as user_photos (add res/xml/files.xml)
    
    - hook up files.xml to FileProvider (edit manifests/AndroidManifest.xml)
    
    - initialize takePhoto property in HomeFragment (edit java/com.aqchen/filterfiesta/ui/home/HomeFragment.kt)

--- app/src/main/java/com/aqchen/filterfiesta/ui/home/HomeFragment.kt
+++ app/src/main/java/com/aqchen/filterfiesta/ui/home/HomeFragment.kt
@@ -6,7 +6,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.FileProvider
 import com.aqchen.filterfiesta.R
+import java.io.File
+import java.util.Date
 
 class HomeFragment : Fragment() {
 
@@ -16,6 +20,24 @@ class HomeFragment : Fragment() {
 
     private lateinit var viewModel: HomeViewModel
 
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+    private val takePhoto = registerForActivityResult(
+        ActivityResultContracts.TakePicture()
+    ) { tookPhoto: Boolean ->
+        if (tookPhoto && photoName != null){
+            HomeViewModel.updatePhoto { oldPhoto ->
+                oldPhoto.copy(photoFileName = photoName)
+            }
+        }
+    }
+
+    private var photoName: String? = null
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -23,10 +45,20 @@ class HomeFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
-        // TODO: Use the ViewModel
+    override fun OnViewCreated(view: View, savedInstanceState: Bundle?){
+        super.onViewCreated(view, savedInstanceState)
+        binding.apply{
+            userCamera.setOnClickListener{
+                photoName = "IMG_${Date()}.JPG"
+                val photoFile = File(requireContext().applicationContext.filesDir,photoName)
+                val photoUri = FileProvider.getUriForFile(
+                    requireContext(),
+                    "com.aqchen.filterfiesta.fileprovider",
+                    photoFile
+                )
+                takePhoto.launch(photoUri)
+            }
+        }
     }
 
 }
